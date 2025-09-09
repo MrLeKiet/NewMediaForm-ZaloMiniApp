7b3b12a7d06a069630239fc9a390323bd601ea89
MrLeKiet <kietsvis@gmail.com>
2025-09-09 14:31:20 +0700

    Setup repo

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import { Page } from "zmp-ui";
+
+function HomePage() {
+  return (
+    <Page style={{ paddingTop: 'var(--safe-top)', paddingBottom: 'var(--safe-bottom)' }}>
+    </Page>
+  );
+}
+
+export default HomePage;
